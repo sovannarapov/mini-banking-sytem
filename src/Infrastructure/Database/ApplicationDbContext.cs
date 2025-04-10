82b4e4bf2261b86f5830de955231836d2e35d50f
sovannarapov <sovannarapov9@gmail.com>
2025-04-10 20:09:44 +0700

    feat: add ApplicationDbContext.cs

---
+++ src/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Application.Abstractions.Data;
+using Domain.Accounts;
+using Domain.Transactions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : DbContext(options), IApplicationDbContext
+{
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+}
