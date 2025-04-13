ad34caa110da4768a76d4245fa828af6b8d0ff39
sovannarapov <sovannarapov9@gmail.com>
2025-04-13 18:01:37 +0700

    feat: add CreateAccountRequest and AccountResponse

---
+++ src/Application/Dtos/Account/CreateAccountRequest.cs
@@ -0,0 +1,9 @@
+using Domain.Accounts;
+
+namespace Application.Dtos.Account;
+
+public sealed class CreateAccountRequest
+{
+    public string OwnerName { get; set; }
+    public AccountType AccountType { get; set; } = AccountType.Savings;
+};
