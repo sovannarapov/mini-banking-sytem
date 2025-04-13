dcf5bf3ffb31643f17550ddcd181542047e2c7d1
sovannarapov <sovannarapov9@gmail.com>
2025-04-13 23:54:13 +0700

    feat: add GetAccountByIdQueryHandler

---
+++ src/Application/Features/Accounts/GetById/GetAccountByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dtos.Account;
+using Domain.Accounts;
+using Domain.Extensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Application.Features.Accounts.GetById;
+
+internal sealed class GetAccountByIdQueryHandler(IApplicationDbContext context) : IQueryHandler<GetAccountByIdQuery, AccountResponse>
+{
+    public async Task<Result<AccountResponse>> Handle(GetAccountByIdQuery query, CancellationToken cancellationToken)
+    {
+        AccountResponse? account = await context.Accounts
+            .AsNoTracking()
+            .Where(acc => acc.Id == query.AccountId)
+            .Select(acc => new AccountResponse(acc.Id, acc.OwnerName, acc.AccountType.GetDisplayName(), acc.Balance, acc.AccountNumber, acc.CreatedAt))
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (account is null)
+        {
+            return Result.Failure<AccountResponse>(AccountError.NotFound(query.AccountId));
+        }
+
+        return account;
+    }
+}
