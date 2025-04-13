5e9c48fe6aef38a1694d551bc051a0d025c2ed52
sovannarapov <sovannarapov9@gmail.com>
2025-04-13 23:40:00 +0700

    refactor: update fields

--- src/Domain/Accounts/Account.cs
+++ src/Domain/Accounts/Account.cs
@@ -8,10 +8,8 @@ public sealed class Account
     public Guid Id { get; set; }
     public string OwnerName { get; set; }
     public string AccountNumber { get; set; }
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public AccountType AccountType { get; set; }
-    public double Balance { get; set; }
-    public DateTime CreatedAt { get; set; }
-
-    public ICollection<Transaction> Transactions { get; set; } = [];
+    public AccountType AccountType { get; set; } = AccountType.Savings;
+    public decimal Balance { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public List<Transaction> Transactions { get; set; } = [];
 }
