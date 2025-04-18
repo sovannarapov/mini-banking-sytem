8fb72ac00ef767c8f4ebed8a3fa5b8df3360798b
sovannarapov <sovannarapov9@gmail.com>
2025-04-18 11:52:50 +0700

    refactor: improve TransferRequest.cs

--- src/Application/Dtos/Transaction/TransferRequest.cs
+++ src/Application/Dtos/Transaction/TransferRequest.cs
@@ -1,8 +1,3 @@
 namespace Application.Dtos.Transaction;
 
-public sealed class TransferRequest
-{
-    public Guid AccountId { get; set; }
-    public string TargetAccountNumber { get; set; }
-    public decimal Amount { get; set; }
-}
+public sealed record TransferRequest(Guid AccountId, string TargetAccountNumber, decimal Amount);
