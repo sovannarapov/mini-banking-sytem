d9df4651da1f7a69fcb302efa36be63b0bf29b40
sovannarapov <sovannarapov9@gmail.com>
2025-04-18 20:47:01 +0700

    feat: add GetTransactionHistoryQuery.cs

---
+++ src/Application/Features/Transactions/History/GetTransactionHistoryQuery.cs
@@ -0,0 +1,14 @@
+using Application.Abstractions.Messaging;
+using Domain.Transactions;
+
+namespace Application.Features.Transactions.History;
+
+public record GetTransactionHistoryQuery : IQuery<TransactionHistoryResponse>
+{
+    public Guid AccountId { get; init; }
+    public TransactionType? Type { get; init; }
+    public DateTime? FromDate { get; init; }
+    public DateTime? ToDate { get; init; }
+    public int Page { get; init; } = 1;
+    public int PageSize { get; init; } = 10;
+}
