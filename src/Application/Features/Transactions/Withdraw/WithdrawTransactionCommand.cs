271795db653a6d5fd7bcd824d8edc07339ffa64c
sovannarapov <sovannarapov9@gmail.com>
2025-04-18 12:17:34 +0700

    refactor: improve WithdrawTransactionCommand.cs

--- src/Application/Features/Transactions/Withdraw/WithdrawTransactionCommand.cs
+++ src/Application/Features/Transactions/Withdraw/WithdrawTransactionCommand.cs
@@ -3,8 +3,4 @@ using Application.Dtos.Transaction;
 
 namespace Application.Features.Transactions.Withdraw;
 
-public sealed class WithdrawTransactionCommand : ICommand<TransactionResponse>
-{
-    public Guid AccountId { get; set; }
-    public decimal Amount { get; set; }
-}
+public sealed record WithdrawTransactionCommand(Guid AccountId, decimal Amount) : ICommand<TransactionResponse>;
