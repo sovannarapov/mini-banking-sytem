c2dd7f5a28c21d476b297fd8471d959aeb064607
sovannarapov <sovannarapov9@gmail.com>
2025-04-20 18:21:01 +0700

    feat: add AddMappings service

--- src/Web.Api/DependencyInjection.cs
+++ src/Web.Api/DependencyInjection.cs
@@ -1,9 +1,13 @@
+using Web.Api.Extensions;
+
 namespace Web.Api;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
+        services.AddMappings(typeof(DependencyInjection).Assembly);
+        
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
 
