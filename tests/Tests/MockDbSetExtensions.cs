21e5eb1efa949a1313d67490898d4b5637787b23
sovannarapov <sovannarapov9@gmail.com>
2025-04-21 22:23:18 +0700

    feat: add MockDbSetExtensions.cs

---
+++ tests/Tests/MockDbSetExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Tests;
+
+public static class MockDbSetExtensions
+{
+    public static DbSet<T> BuildMockDbSet<T>(this IQueryable<T> data) where T : class
+    {
+        var mockSet = new Mock<DbSet<T>>();
+        mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+        mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+        mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+        mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+        
+        return mockSet.Object;
+    }
+}
