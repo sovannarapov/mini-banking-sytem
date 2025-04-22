f42bdddc4c52d43f3830b1cbcb49999e56a0e638
sovannarapov <sovannarapov9@gmail.com>
2025-04-22 16:48:06 +0700

    feat: add AccountBaseTest.cs

---
+++ tests/Tests/Features/Accounts/AccountBaseTest.cs
@@ -0,0 +1,37 @@
+using Application.Abstractions.Data;
+using Application.Interfaces;
+using Domain.Accounts;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Tests.Features.Accounts;
+
+public abstract class AccountBaseTest
+{
+    protected readonly Mock<IApplicationDbContext> MockDbContext;
+    protected readonly Mock<IAccountNumberGenerator> MockAccountNumberGenerator;
+    protected readonly Mock<TimeProvider> MockTimeProvider;
+    protected readonly Mock<IGuidGenerator> MockGuidGenerator;
+
+    protected readonly Guid FixedAccountId = Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
+    protected readonly DateTime FixedDate = new(2024, 1, 1, 12, 0, 0, DateTimeKind.Utc);
+    protected const string FixedAccountNumber = "0123456789";
+
+    protected AccountBaseTest()
+    {
+        IEnumerable<Account> accounts = [];
+        DbSet<Account> accountsDbSet = accounts.AsQueryable().BuildMockDbSet();
+
+        MockDbContext = new Mock<IApplicationDbContext>();
+        MockDbContext.Setup(applicationDbContext => applicationDbContext.Accounts).Returns(accountsDbSet);
+
+        MockTimeProvider = new Mock<TimeProvider>();
+        MockTimeProvider.Setup(timeProvider => timeProvider.GetUtcNow()).Returns(FixedDate);
+
+        MockAccountNumberGenerator = new Mock<IAccountNumberGenerator>();
+        MockAccountNumberGenerator.Setup(accountNumberGenerator => accountNumberGenerator.Generate()).Returns(FixedAccountNumber);
+
+        MockGuidGenerator = new Mock<IGuidGenerator>();
+        MockGuidGenerator.Setup(guidGenerator => guidGenerator.NewGuid()).Returns(FixedAccountId);
+    }
+}
