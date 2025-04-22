59e1d33a9de5540cb81343c6c7f6681d3ae9a195
sovannarapov <sovannarapov9@gmail.com>
2025-04-22 16:44:37 +0700

    feat: add new Error method

--- src/Domain/Accounts/AccountError.cs
+++ src/Domain/Accounts/AccountError.cs
@@ -8,16 +8,15 @@ public static class AccountError
     {
         if (accountId is null && accountNumber is null)
         {
-            return Error.NotFound("Accounts.NotFound", "Account was not found.");
+            return Error.NotFound($"{nameof(Account)}.NotFound", "Account was not found.");
         }
 
         return accountId.HasValue
-            ? Error.NotFound("Accounts.NotFound", $"Account with ID '{accountId}' was not found.")
-            : Error.NotFound("Accounts.NotFound", $"Account with number '{accountNumber}' was not found.");
+            ? Error.NotFound($"{nameof(Account)}.NotFound", $"Account with ID '{accountId}' was not found.")
+            : Error.NotFound($"{nameof(Account)}.NotFound", $"Account with number '{accountNumber}' was not found.");
     }
 
-    public static Error Required(string field)
-    {
-        return Error.Failure("Accounts.Required", $"Field '{field}' is required.");
-    }
+    public static Error Required(string fieldName) => Error.Failure(
+        $"{nameof(Account)}.Required", 
+        $"{fieldName} is required.");
 }
