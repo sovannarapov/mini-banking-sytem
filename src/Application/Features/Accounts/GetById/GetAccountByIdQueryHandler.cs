6449b91b5d57032e2536ec622eeefd895c719585
sovannarapov <sovannarapov9@gmail.com>
2025-04-24 00:07:32 +0700

    refactor: improve GetAccountByIdQueryHandler.cs

--- src/Application/Features/Accounts/GetById/GetAccountByIdQueryHandler.cs
+++ src/Application/Features/Accounts/GetById/GetAccountByIdQueryHandler.cs
@@ -8,7 +8,7 @@ using Shared;
 
 namespace Application.Features.Accounts.GetById;
 
-internal sealed class GetAccountByIdQueryHandler(IApplicationDbContext context)
+public sealed class GetAccountByIdQueryHandler(IApplicationDbContext context)
     : IQueryHandler<GetAccountByIdQuery, AccountResponse>
 {
     public async Task<Result<AccountResponse>> Handle(GetAccountByIdQuery query, CancellationToken cancellationToken)
@@ -16,7 +16,7 @@ internal sealed class GetAccountByIdQueryHandler(IApplicationDbContext context)
         AccountResponse? account = await context.Accounts
             .AsNoTracking()
             .Where(acc => acc.Id == query.AccountId)
-            .Select(acc => new AccountResponse(acc.Id, acc.OwnerName, acc.AccountType.GetDisplayName(), 
+            .Select(acc => new AccountResponse(acc.Id, acc.OwnerName, acc.AccountType.GetDisplayName(),
                 acc.Balance, acc.AccountNumber, acc.CreatedAt))
             .SingleOrDefaultAsync(cancellationToken);
 
