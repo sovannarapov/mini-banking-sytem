e079e924e88591ce0f9cf351a231aca18693520f
sovannarapov <sovannarapov9@gmail.com>
2025-04-23 14:45:10 +0700

    feat: add GetAccountQueryHandlerTests.cs

---
+++ tests/Tests/Features/Accounts/Get/GetAccountQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+using Application.Dtos.Account;
+using Application.Features.Accounts.Get;
+using Domain.Accounts;
+using FluentAssertions;
+using Shared;
+
+namespace Tests.Features.Accounts.Get;
+
+public class GetAccountQueryHandlerTests : AccountBaseTest
+{
+    [Fact]
+    public async Task Handle_ShouldReturnAccount_WhenAccountExists()
+    {
+        // Arrange
+        Accounts.Add(new Account
+        {
+            Id = Guid.NewGuid(),
+            OwnerName = "Alice",
+            AccountType = AccountType.Checking,
+            Balance = 500,
+            AccountNumber = "1234567890",
+            CreatedAt = FixedDate
+        });
+
+        Accounts.Add(new Account
+        {
+            Id = Guid.NewGuid(),
+            OwnerName = "Bob",
+            AccountType = AccountType.Savings,
+            Balance = 1000,
+            AccountNumber = "0987654321",
+            CreatedAt = FixedDate
+        });
+
+        var query = new GetAccountQuery();
+
+        var getAccountQueryHandler = new GetAccountQueryHandler(MockDbContext.Object);
+
+        // Act
+        Result<List<AccountResponse>> result = await getAccountQueryHandler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().HaveCount(2);
+    }
+}
