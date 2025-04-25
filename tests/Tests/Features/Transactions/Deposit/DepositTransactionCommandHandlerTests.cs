67bc746bc43209e34ae2e7533440866f8edbfd26
sovannarapov <sovannarapov9@gmail.com>
2025-04-25 11:59:18 +0700

    feat: add Handle_ShouldCallUpdateBalance_AndProcessDeposit_WhenCommandIsValid test

--- tests/Tests/Features/Transactions/Deposit/DepositTransactionCommandHandlerTests.cs
+++ tests/Tests/Features/Transactions/Deposit/DepositTransactionCommandHandlerTests.cs
@@ -1,6 +1,7 @@
 using Application.Dtos.Transaction;
 using Application.Features.Transactions.Deposit;
 using Domain.Accounts;
+using Domain.Extensions;
 using Domain.Transactions;
 using FluentAssertions;
 using Moq;
@@ -57,5 +58,33 @@ public sealed class DepositTransactionCommandHandlerTests : TransactionBaseTest
         // Assert
         result.IsFailure.Should().BeTrue();
         result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallUpdateBalance_AndProcessDeposit_WhenCommandIsValid()
+    {
+        // Arrange
+        var command = new DepositTransactionCommand(FixedAccountId, FixedAmount);
+        Account account = new();
+
+        MockAccountService
+            .Setup(accountService =>
+                accountService.GetAccountByIdAsync(FixedAccountId, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(account);
+
+        var expectedResponse = new TransactionResponse(Guid.NewGuid(), FixedAccountId, TransactionType.Deposit.GetDisplayName(), FixedAmount, FixedAccountNumber, FixedDate);
+
+        MockTransactionService.Setup(s => s.ProcessDepositAsync(account, FixedAmount, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Success(expectedResponse));
+
+        // Act
+        Result<TransactionResponse> result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(expectedResponse);
+
+        MockAccountService.Verify(s => s.UpdateBalance(account, FixedAmount), Times.Once);
+        MockTransactionService.Verify(s => s.ProcessDepositAsync(account, FixedAmount, It.IsAny<CancellationToken>()), Times.Once);
     }
 }
