7d96ca0a4f80b57f2d4c015e1693de9d7e9da36d
sovannarapov <sovannarapov9@gmail.com>
2025-04-25 11:58:49 +0700

    refactor: update TransactionBaseTest.cs

--- tests/Tests/Features/Transactions/TransactionBaseTest.cs
+++ tests/Tests/Features/Transactions/TransactionBaseTest.cs
@@ -1,12 +1,8 @@
 using Application.Common.Interfaces;
-using Application.Dtos.Transaction;
-using Domain.Accounts;
-using Domain.Extensions;
 using Domain.Transactions;
 using Microsoft.EntityFrameworkCore;
 using MockQueryable.Moq;
 using Moq;
-using Shared;
 using Tests.Features.Accounts;
 
 namespace Tests.Features.Transactions;
@@ -15,7 +11,6 @@ public abstract class TransactionBaseTest : AccountBaseTest
 {
     protected const decimal FixedAmount = 1000;
     protected const decimal InvalidFixedAmount = -1000m;
-    protected readonly Guid FixedTransactionId = Guid.Parse("bbbbbbbb-cccc-dddd-eeee-ffffffffffff");
     protected readonly Mock<IAccountService> MockAccountService;
     protected readonly Mock<ITransactionService> MockTransactionService;
     protected readonly Mock<IValidationService> MockValidationService;
@@ -34,28 +29,7 @@ public abstract class TransactionBaseTest : AccountBaseTest
         MockGuidGenerator.Setup(guidGenerator => guidGenerator.NewGuid()).Returns(FixedAccountId);
 
         MockValidationService = new Mock<IValidationService>();
-        MockValidationService.Setup(validationService => validationService.ValidateDepositAmount(It.IsAny<decimal>()))
-            .Returns(Result.Success);
-
         MockAccountService = new Mock<IAccountService>();
-        MockAccountService
-            .Setup(accountService =>
-                accountService.GetAccountByIdAsync(FixedAccountId, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new Account());
-        MockAccountService
-            .Setup(accountService => accountService.UpdateBalance(It.IsAny<Account>(), It.IsAny<decimal>()))
-            .Callback<Account, decimal>((acc, amount) => acc.Balance += amount);
-
         MockTransactionService = new Mock<ITransactionService>();
-        MockTransactionService
-            .Setup(t => t.ProcessDepositAsync(It.IsAny<Account>(), It.IsAny<decimal>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new TransactionResponse(
-                FixedTransactionId,
-                FixedAccountId,
-                TransactionType.Deposit.GetDisplayName(),
-                FixedAmount,
-                FixedAccountNumber,
-                FixedDate
-            ));
     }
 }
