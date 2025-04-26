61b5d87909be03bf347ecfda1d6e94e4443039ea
sovannarapov <sovannarapov9@gmail.com>
2025-04-26 14:08:17 +0700

    refactor: update IAccountService.cs

--- src/Application/Common/Interfaces/IAccountService.cs
+++ src/Application/Common/Interfaces/IAccountService.cs
@@ -1,9 +1,11 @@
 using Domain.Accounts;
+using Shared;
 
 namespace Application.Common.Interfaces;
 
 public interface IAccountService
 {
     Task<Account?> GetAccountByIdAsync(Guid accountId, CancellationToken cancellationToken);
-    void UpdateBalance(Account account, decimal amount);
+    void Deposit(Account account, decimal amount);
+    Result Withdraw(Account account, decimal amount);
 }
