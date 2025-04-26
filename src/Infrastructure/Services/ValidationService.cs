72969d5a2c0e9a89fb95d4a697f31b63f68f8b8b
sovannarapov <sovannarapov9@gmail.com>
2025-04-26 14:13:12 +0700

    refactor: update ValidationService.cs

--- src/Infrastructure/Services/ValidationService.cs
+++ src/Infrastructure/Services/ValidationService.cs
@@ -1,4 +1,5 @@
 using Application.Common.Interfaces;
+using Application.Dtos.Transaction;
 using Domain.Constants;
 using Domain.Transactions;
 using Serilog;
@@ -15,11 +16,26 @@ public class ValidationService(ILogger logger) : IValidationService
             case <= TransactionConstants.MinTransactionAmount:
                 logger.Warning("Invalid deposit amount: {Amount}", amount);
                 return Result.Failure(TransactionError.InvalidAmount(amount));
-            case > TransactionConstants.MaxDepositAmount:
+            case > TransactionConstants.MaxTransactionAmount:
                 logger.Warning("Deposit amount exceeds maximum limit: {Amount}", amount);
                 return Result.Failure(TransactionError.ExceedsMaximumLimit(amount));
             default:
                 return Result.Success();
         }
+    }
+
+    public Result ValidateWithdrawAmount(decimal currentBalance, decimal amount)
+    {
+        if (amount <= 0)
+        {
+            return Result.Failure(TransactionError.InvalidAmount(amount));
+        }
+
+        if (currentBalance < amount)
+        {
+            return Result.Failure(TransactionError.InsufficientBalance(amount, currentBalance));
+        }
+
+        return Result.Success();
     }
 }
